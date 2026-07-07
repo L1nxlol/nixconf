4d4a6294ca495ec898a375c9e2ad7f3b006509b9
user <DJTrump@cumallover.me>
2026-07-07 18:58:01 +0200

    fixed a typo

--- home/zsh/zsh.nix
+++ home/zsh/zsh.nix
@@ -27,7 +27,7 @@ home.file.".p10k.zsh".source = ./p10k.zsh;
       sreboot = "systemctl soft-reboot";
       off = "shutdown 0";
       fastfetch = "fastfetch";
-      update = "sudo nix flake update ~/Nix";
+      update = "sudo nix flake update --flake ~/Nix";
     };
   };
 }
