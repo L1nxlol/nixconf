970c15fbed753cdb87cf614c9f36e7dbe4f692c6
user <DJTrump@cumallover.me>
2026-07-12 16:01:28 +0200

    fixed .qml lsp

--- home/nvim/nvchad/lua/configs/lspconfig.lua
+++ home/nvim/nvchad/lua/configs/lspconfig.lua
@@ -6,8 +6,8 @@ vim.lsp.config.qmlls = {
   root_dir = require("lspconfig.util").root_pattern(".git"),
 }
 
-local servers = { "html", "cssls", "qmlls" }
+local servers = { "html", "cssls"}
 vim.lsp.enable(servers)
-
+-- require("lspconfig").qmlls.setup {}
 -- read :h vim.lsp.config for changing options of lsp servers 
 
