4f2f35550467225edfd817c906816810eeccc19c
user <DJTrump@cumallover.me>
2026-09-01 20:27:18 +0200

    fixed a rebuild error

--- home/obsidian/obsidian.nix
+++ home/obsidian/obsidian.nix
@@ -1,16 +1,16 @@
-{ pkgs, config, ... }:
+{ pkgs, config, obsidianPlugins, ... }:
 
 {
 programs.obsidian = {
   enable = true;
-  vaults."notes" = {
-    target = "Documents/notes";
-    settings = {
-      app.vimMode = true;
-      communityPlugins = [
-        { pkg = pkgs.obsidian-plugins.excalidraw; }
-      ];
-    };
+  vaults."notes".target = "Documents/notes";
+  defaultSettings = {
+    app.vimMode = true;
+    communityPlugins = with pkgs.obsidianPlugins; [
+      obsidian-excalidraw-plugin
+      chemedit
+      obsidian-style-settings
+    ];
   };
 };
 }
