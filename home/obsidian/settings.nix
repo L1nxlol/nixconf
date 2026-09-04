6ba01cd6489ec167e30dfdc4e6b63aa91fb71021
user <DJTrump@cumallover.me>
2026-09-04 19:22:33 +0000

    settings

--- home/obsidian/settings.nix
+++ home/obsidian/settings.nix
@@ -1,5 +1,21 @@
 { pkgs, config, obsidianPlugins, ... }:
 
 {
+  programs.obsidian.defaultSettings = {
+    app = {
+      showLineNumber = true;
+      showInlineTitle = false;
+      confirmFileDeletion = false;
+      deletedFiles = "trash";
+      spellcheck = true;
+      spellcheckLanguages = [
+        "pl"
+        "en-US"
+      ];
+    };
 
+    appearance = {
+      accentColor = "#ffffff";
+    };
+  };
 }
