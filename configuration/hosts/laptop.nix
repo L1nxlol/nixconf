04ca024660049c3b1402ffb01621e77e78c4f635
user <DJTrump@cumallover.me>
2026-09-05 13:07:42 +0000

    ly battery only shows on #laptop

--- configuration/hosts/laptop.nix
+++ configuration/hosts/laptop.nix
@@ -8,6 +8,8 @@
     ../../pkgs/laptop.nix 
   ];
 
+  services.displayManager.ly.settings.battery_id = "BAT_1";
+
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [
     53317 # Local Send
