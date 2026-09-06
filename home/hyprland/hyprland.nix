4bff6e41d5281e045a936913c0659fb2dea05a96
user <DJTrump@cumallover.me>
2026-09-06 14:05:46 +0200

    removed dynamic-cursors (broken repo)

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -3,6 +3,9 @@
 let
   hyprglass = pkgs.callPackage ../../cPkgs/hyprglass.nix {
     mkHyprlandPlugin = pkgs.hyprlandPlugins.mkHyprlandPlugin;
+  };  
+  dynamic-cursors = pkgs.callPackage ../../cPkgs/dynamic-cursors.nix {
+    mkHyprlandPlugin = pkgs.hyprlandPlugins.mkHyprlandPlugin;
   };
 in
 {
@@ -13,5 +16,8 @@ in
       mkdir -p /home/user/Nix/home/hyprland/plugins
       ln -sfn ${hyprglass}/lib/libhyprglass.so \
         /home/user/Nix/home/hyprland/plugins/hyprglass.so
-    '';
+      '';
 }
+
+      # ln -sfn ${dynamic-cursors}/lib/libdynamic-cursors.so \
+        # /home/user/Nix/home/hyprland/plugins/dynamic-cursors.so
