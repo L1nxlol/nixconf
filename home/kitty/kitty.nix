88065fe6fc336ac9b8728613a994a89b9874f81c
user <DJTrump@cumallover.me>
2026-09-06 19:47:15 +0200

    kitty moved over to .nix

--- home/kitty/kitty.nix
+++ home/kitty/kitty.nix
@@ -1,4 +1,24 @@
 { config, pkgs, ... }:
 {
-  xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
+  programs.kitty = {
+    enable = true;
+
+    font = {
+      name = "GeistMono Nerd Font Mono";
+      size = 10;
+    };
+
+    settings = {
+      background_opacity = 0.6;
+      confirm_os_window_close = 0;
+      scrollbar_handle_opacity = 0; 
+      scrollbar_track_opacity = 0;
+
+      cursor_shape = "beam";
+      cursor_trail = 1;
+      cursor_trail_decay = "0.1  0.3";
+
+      symbol_map = "U+e000-U+f8ff Symbols Nerd Font Mono";
+    };
+  };
 }
