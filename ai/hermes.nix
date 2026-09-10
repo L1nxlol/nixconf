460a936924b4809311b6c6acd1e78e085e616a93
user <DJTrump@cumallover.me>
2026-09-10 23:06:41 +0200

    uhhh

--- ai/hermes.nix
+++ ai/hermes.nix
@@ -7,9 +7,25 @@
     backend.mode = "none";
 
     settings = {
-      provider = "custom";
-      model.base_url = "http://127.0.0.1:8000/v1";
-      model.default = "Qwen3.6-35B-A3B-UD-Q6_K_XL";
+      model = {
+        provider = "custom";
+        base_url = "http://127.0.0.1:8080/v1";
+        default = "Qwen3.6-35B-A3B-UD-Q6_K_XL";
+      };
+
+      model_aliases = {
+        qwen36 = {
+          model = "Qwen3.6-35B-A3B-UD-Q6_K_XL";
+          provider = "custom";
+          base_url = "http://127.0.0.1:8080/v1";
+        };
+
+        qwen3vl = {
+          model = "Qwen3VL-8B-Instruct-Q4_K_M";
+          provider = "custom";
+          base_url = "http://127.0.0.1:8080/v1";
+        };
+      };
 
       toolsets = [
         "web"
--- ai/llama.nix
+++ ai/llama.nix
@@ -8,8 +8,8 @@
   systemd.user.services.llama-server = {
     serviceConfig = {
       ExecStart = "${pkgs.llama-cpp-rocm}/bin/llama-server \
-        -m /var/lib/models/current.gguf \
-        --alias Qwen3.6-35B-A3B-UD-Q6_K_XL \
+        --models-dir /var/lib/models \
+        --models-max 1 \
         --host 127.0.0.1 \
         --port 8080 \
         --ctx-size 65536 \
@@ -19,7 +19,6 @@
         -ngl 99 \
         -ot ${"''"}.ffn_.*_exps.=CPU${"''"}";
 
-      # hardening
       NoNewPrivileges = true;
       ProtectSystem = "strict";
       ProtectHome = "read-only";
@@ -31,19 +30,15 @@
       RestrictNamespaces = true;
       LockPersonality = true;
       MemoryDenyWriteExecute = false;
+
       ReadOnlyPaths = [ "/var/lib/models" ];
 
-      # ROCm device access
-      # Required don't drop these
       DeviceAllow = [
         "/dev/kfd rw"
         "/dev/dri rw"
       ];
-      # SupplementaryGroups = [ "render" "video" ];
     };
+
     wantedBy = [ "default.target" ];
   };
 }
-
-# switch model
-# ln -sfn /var/lib/models/new.gguf /var/lib/models/current.gguf && systemctl --user restart llama-server
