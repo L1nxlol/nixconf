adf17488c13a4b8d608d8f6f6ebd1e279fcccb18
user <DJTrump@cumallover.me>
2026-09-10 23:33:58 +0200

    new zen flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     };
 
     zen-browser = {
-      url = "github:youwen5/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
