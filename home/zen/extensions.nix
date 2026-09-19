4bf57e156737069c7204caad64b7179b747d2a39
user <DJTrump@cumallover.me>
2026-09-19 22:09:54 +0200

    added vimium

--- home/zen/extensions.nix
+++ home/zen/extensions.nix
@@ -31,5 +31,10 @@
       install_url = "https://addons.mozilla.org/firefox/downloads/latest/return_youtube_dislikes/latest.xpi";
       installation_mode = "force_installed";
     };
+
+    "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+      installation_mode = "force_installed";
+    };
   };
 }
