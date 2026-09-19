e359663908b1ab673b94478daa7208428e3c0f32
user <DJTrump@cumallover.me>
2026-09-19 22:38:07 +0200

    workning in tty

--- home/zsh/zsh.nix
+++ home/zsh/zsh.nix
@@ -10,25 +10,33 @@ home.file.".p10k.zsh".source = ./p10k.zsh;
       plugins = [ "git" "sudo" ];
     };
 
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
-
     initContent = ''
-    [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      if [[ "$TERM" == "linux" ]]; then
+        setopt prompt_subst
+        _tty_git() {
+          local b ab s
+          b=$(git symbolic-ref --short HEAD 2>/dev/null) || return
+          ab=$(git rev-list --left-right --count '@{u}...HEAD' 2>/dev/null | awk '{print "-"$1" +"$2}')
+          git diff --quiet 2>/dev/null && s=ok || s='%F{red}dirty%F{cyan}'
+          print -n "> $b $ab $s "
+        }
 
-    clear-scrollback() {
-      printf '\033[3J\033[H\033[2J'
-      zle reset-prompt
-      zle redisplay
-    }
-    zle -N clear-scrollback
-    bindkey '^L' clear-scrollback
-    bindkey '^H' backward-kill-word
+        # PROMPT='%F{cyan}%(?.ok.%?) > %T > %1~ $(_tty_git)%f
+    # %F{cyan}->%f '
+    PROMPT='%F{cyan}%~ ->%f '
+      else
+        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      fi
+
+      clear-scrollback() {
+        printf '\033[3J\033[H\033[2J'
+        zle reset-prompt
+        zle redisplay
+      }
+      zle -N clear-scrollback
+      bindkey '^L' clear-scrollback
+      bindkey '^H' backward-kill-word
     '';
 
     shellAliases = {
