24757225a32055735c240a886f655a890de89a84
user <DJTrump@cumallover.me>
2026-09-21 22:04:09 +0200

    more shortcuts + spacing

--- home/nvim/lua/plugins/startpage.lua
+++ home/nvim/lua/plugins/startpage.lua
@@ -19,6 +19,10 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("e", "  New file", "<Cmd>ene<CR>"),
       dashboard.button("f", "  Find file", "<Cmd>Telescope find_files<CR>"),
+      dashboard.button("h", "  Hidden files", "<Cmd>Telescope find_files hidden=true<CR>"),
+      dashboard.button("r", "  Recent file", "<Cmd>Telescope oldfiles<CR>"),
+      dashboard.button("p", "  Lazy", "<Cmd>Lazy<CR>"),
+      dashboard.button("n", "  Nix", ":e/home/user/Nix/<CR>"),
       dashboard.button("q", "  Quit", "<Cmd>qa<CR>"),
     }
     require("alpha").setup(dashboard.opts)
